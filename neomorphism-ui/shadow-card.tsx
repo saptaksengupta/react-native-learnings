64dda7c53211c925dc5b4b7d160206a1b2690a05
saptak sengupta <deeps.sengupta@gmail.com>
2021-02-20 16:28:17 +0530

    [main] neomorphism initiation ... 🍻

---
+++ neomorphism-ui/shadow-card.tsx
@@ -0,0 +1,18 @@
+import * as React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const ShadowCard = () => {
+    return (
+        <View>
+            <Text>Shadow Card</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    
+});
+
+export {
+    ShadowCard
+}
